2c7948943169558a31c8141765b7d695ad3fad2b
OoHuakoO <komsark_@hotmail.co.th>
2024-08-06 22:16:08 +0700

    add create fmctoken and remove fcmToken

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -6,9 +6,9 @@ async function register(req, res, next) {
   try {
     console.log("start register.controller  req body :", req?.body);
 
-    const { email, password } = req?.body;
+    const { email, password, fcmToken } = req?.body;
 
-    if (!(email && password)) {
+    if (!(email && password && fcmToken)) {
       return res.json({
         data: "all input is required",
         status: 400,
@@ -23,6 +23,9 @@ async function register(req, res, next) {
       });
     }
     const userRegister = await userService.register(email, password);
+
+    await userService.createFcmToken(userRegister._id, fcmToken);
+
     return res.json({
       data: userRegister,
       status: 200,
@@ -41,9 +44,9 @@ async function login(req, res, next) {
       JSON.stringify(req?.body, null, 2)
     );
 
-    const { email, password } = req?.body;
+    const { email, password, fcmToken } = req?.body;
 
-    if (!(email && password)) {
+    if (!(email && password && fcmToken)) {
       return res.json({
         data: "all input is required",
         status: 400,
@@ -53,6 +56,7 @@ async function login(req, res, next) {
     const user = await userService.findByEmail(email);
     if (user && (await bcrypt.compare(password, user.password))) {
       const userLogin = await userService.login(user);
+      await userService.createFcmToken(userLogin._id, fcmToken);
       return res.json({
         data: userLogin,
         status: 200,
@@ -70,7 +74,45 @@ async function login(req, res, next) {
   }
 }
 
+async function removeFcmToken(req, res, next) {
+  try {
+    console.log(
+      "start removeFcmToken.controller req body :",
+      JSON.stringify(req?.body, null, 2)
+    );
+
+    const { fcmToken } = req.body;
+
+    const userId = req.user.user_id;
+
+    if (!(fcmToken && userId)) {
+      return res.json({
+        data: "fcmToken and userId are required",
+        status: 400,
+      });
+    }
+
+    const resultRemove = await userService.removeFcmToken(userId, fcmToken);
+
+    if (resultRemove.deletedCount === 0) {
+      return res.json({ data: "fcmToken not found", status: 404 });
+    }
+
+    console.log(`Successfully removed fcmToken ${fcmToken} for user ${userId}`);
+
+    res.json({ data: "fcmToken removed successfully", status: 200 });
+  } catch (err) {
+    console.error(
+      `removeFcmToken.controller error while removeFcmToken`,
+      err.message
+    );
+    res.json({ data: err.message, status: 500 });
+    next(err);
+  }
+}
+
 module.exports = {
   register,
   login,
+  removeFcmToken,
 };
--- src/routes/user.route.js
+++ src/routes/user.route.js
@@ -1,9 +1,12 @@
 const express = require("express");
 const router = express.Router();
 const userController = require("../controllers/user.controller");
+const verifyToken = require("../middlewares/auth.middleware");
 
 router.post("/register", userController.register);
 
 router.post("/login", userController.login);
+
+router.post("/remove-fcm-token", verifyToken, userController.removeFcmToken);
 
 module.exports = router;
