97e2166b6c38adb418c694daace9dd3586494cd8
OoHuakoO <komsark_@hotmail.co.th>
2024-08-11 17:48:00 +0700

    add sharp resize pic

--- src/utils/uploadFile.js
+++ src/utils/uploadFile.js
@@ -5,6 +5,7 @@ const {
   uploadBytesResumable,
   getDownloadURL,
 } = require("firebase/storage");
+const sharp = require("sharp");
 const config = require("../configs/firebase.config");
 const { giveCurrentDateTime } = require("./time");
 
@@ -12,19 +13,28 @@ initializeApp(config.firebaseConfig);
 
 const storage = getStorage();
 
+async function compressImage(file) {
+  return await sharp(file.buffer)
+    .resize({ width: 800, height: 800 })
+    .toBuffer();
+}
+
 async function uploadFileFirebase(file) {
   try {
     const dateTime = giveCurrentDateTime();
     const metadata = {
       contentType: file.mimetype,
     };
+
+    const compressedBuffer = await compressImage(file);
+
     const storageRef = ref(
       storage,
       `files/${file.originalname + " " + dateTime}`
     );
     const snapshot = await uploadBytesResumable(
       storageRef,
-      file.buffer,
+      compressedBuffer,
       metadata
     );
     const downloadURL = await getDownloadURL(snapshot.ref);
